c9ce1307306c5beed144d34bd814f726119e1a81
Davide <bacilieri.davide@gmail.com>
2024-03-02 16:41:52 +0100

    subsetting error on pi, solved other errors

--- slds_jags.r
+++ slds_jags.r
@@ -1,19 +1,21 @@
 library(rjags)
 
 data = read.csv("data/nascar/dataset.csv")
-names(data) = c("x","y")
+#names(data) = c("x","y")
 
 #plot(data$x, data$y, type="l")
 
 mod_data=NULL
 mod_data$x=data[seq(1,length(data), 100)]
 mod_data$T=length(mod_data$y)
+mod_data$K=4
+mod_data$D=2
 
 mod <- jags.model("jags/slds.bug", mod_data)
 
 update(mod, 1000) # burn-in
 
-chain <- coda.samples(mod , c("l", "y"), n.iter=10000) # sample
+chain <- coda.samples(mod , c("z", "x"), n.iter=10000) # sample
 
 summary(chain)
 plot(chain)
