fd9f8c7c07814dd973e1504fa2453cc4ee258f3b
Davide <bacilieri.davide@gmail.com>
2024-03-06 18:52:12 +0100

    jags structure is ok, got to make Ab, Q and pi into dataframes or similar, or they won't be used by the generating model

--- slds_jags.r
+++ slds_jags.r
@@ -1,4 +1,5 @@
 library(rjags)
+library(ggplot2)
 
 data = read.csv("data/nascar/dataset.csv")
 #names(data) = c("x","y")
@@ -11,13 +12,48 @@ mod_data$T=nrow(mod_data$x)
 mod_data$K=4
 mod_data$D=2
 
-# #load.module("") #if the ddirch is in another module, not sure
+T=mod_data$T
+niter=10
+
 
 mod <- jags.model("jags/slds.bug", mod_data)
 
 update(mod, 1000) # burn-in
 
-chain <- coda.samples(mod , c("z", "x"), n.iter=10000) # sample
+chain <- coda.samples(mod , c("Ab", "Q", "pi"), n.iter=10000) # sample
 
-#summary(chain)
-#plot(chain)
+chain.df = as.data.frame(as.mcmc(chain))
+chain.list = as.list(chain.df)
+chain.list$T=mod_data$T
+chain.list$D=mod_data$D
+chain.list$K=mod_data$K
+chain.list$x0=mod_data$x[1,]
+mod2 <- jags.model("jags/slds-gen.bug", chain.list)
+
+update(mod2, 1000)
+
+chain2 <- coda.samples(mod2 , c("x","z"), n.iter=niter)
+
+#print(chain2)
+
+chain2.df = as.data.frame(as.mcmc(chain2))
+results.list=list()
+for (i in 1:niter) {
+ results.list[[i]]=list()
+ results.list[[i]]$x1=NaN*seq(T)
+ results.list[[i]]$x2=NaN*seq(T)
+ results.list[[i]]$z=NaN*seq(T)
+ for (j in 1:T) {
+  tryCatch({
+  results.list[[i]]$x1[j]=chain2.df[i,paste("x[",j,",1]",sep="")]
+  results.list[[i]]$x2[j]=chain2.df[i,paste("x[",j,",2]",sep="")]
+  results.list[[i]]$z[j]=chain2.df[i,paste("z[",j,"]",sep="")]
+  }, error=function(e){print(i);print(j); print(e);})
+ }
+}
+
+
+
+for (i in 1:niter) {
+ plot(ggplot() + geom_point(aes(results.list[[i]]$x1, results.list[[i]]$x2, col=results.list[[i]]$z)))
+}
