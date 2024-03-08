fa8e942e20a5ca83c906dee668e7695c38698cbf
alessiopitteri <alepitte1902@gmail.com>
2024-03-08 13:52:53 +0100

    let's publish

--- nascar.R
+++ nascar.R
@@ -8,7 +8,7 @@ nascar_full <- fread("data/nascar/dataset.csv", sep = ",")
 names(nascar_full) <- c("x", "y")
 
 # take a subset
-nascar <- nascar_full[seq(1, 2e4, 50)]
+nascar <- nascar_full[seq(1, 4e4+5e3, 200)]
 nrow(nascar)
 
 plot(nascar$x, nascar$y, type = "b"); points(
@@ -20,6 +20,26 @@ dt <- mean(sqrt(diff(nascar$x)^2 + diff(nascar$y)^2))
 A <- matrix(c(cos(dt), sin(dt), -sin(dt), cos(dt)), ncol = 2)
 b <- -A %*% c(1, 0)
 
+states <- c(-1,1)
+z <- sample(states, size = 300, replace = T, prob = c(0.7, 0.3)) 
+theta <- pi/12
+A <- function(z){
+  if(z==1) matrix(c(cos(theta),sin(theta),-sin(theta),cos(theta)), ncol=2)
+  else matrix(c(cos(-theta),sin(-theta),-sin(-theta),cos(-theta)), ncol=2)
+}
+
+x <- rep(list(matrix(c(0,0), ncol=1)), length(z)+1)
+x[[1]] <- matrix(c(1,1), ncol=1)
+for (i in seq_along(z)){
+  x[[i+1]] <- A(z[i]) %*% x[[i]] + rnorm(1, 0, 0.001)
+}
+x[[1]] <- NULL
+
+x <- lapply(x, \(x) x*10)
+data <- as.data.table(transpose(x)) 
+setnames(data, c('x', 'y'))
+plot(data$x, data$y, type = 'l')
+
 sm <- stan_model(
   file = "stan/slds.stan",
   model_name = "SLDS",
@@ -28,15 +48,12 @@ sm <- stan_model(
 
 fit <- sampling(
   sm, data = list(
-    K = 4, M = 2, N = 2, T = nrow(nascar),
-    y = as.matrix(nascar),
-    Mu = list(
-      cbind(diag(1, 2), c(dt, 0)), cbind(A, b),
-      cbind(diag(1, 2), c(-dt, 0)), cbind(A, -b)
-    ),
-    Omega = rep(list(diag(1, 3)), 4), 
-    Psi = rep(list(diag(1 / 3, 2)), 4),
-    nu = rep(3, 4)
+    K = 2, N = 2, T = nrow(data),
+    y = as.matrix(data),
+    Mu = list(cbind(A(1), c(0,0)), cbind(A(2), c(0,0))),
+    Omega = rep(list(matrix(c(1,0,0,0,1,0,0,0,1), ncol=3)), 2), 
+    Psi = rep(list(diag(1/2, 2)), 2),
+    nu = rep(2, 2)
   ),
   chains = 1, iter = 4000, warmup = 1000,
   control = list(adapt_delta = 0.9)
@@ -67,8 +84,8 @@ plot_par <- function(pars) {
 
 par_name <- \(pattern) names(params)[grep(pattern, names(params))]
 
-par_name("pi.4") |> plot_par()
-par_name("A.4") |> plot_par()
-par_name("b.4") |> plot_par()
+par_name("pi.1") |> plot_par()
+par_name("A.1") |> plot_par()
+par_name("b.1") |> plot_par()
 
-params[, par_name("z_"), with = FALSE] |> colSums()
+ss <- params[, par_name("z_"), with = FALSE] |> colSums()
